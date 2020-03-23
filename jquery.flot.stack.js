f516fb4dd06156f80638595c396ca8f0d68351b9
Inna88888 <60699608+Inna88888@users.noreply.github.com>
2020-03-23 17:33:17 +0300

    Add files via upload

--- jquery.flot.stack.js
+++ jquery.flot.stack.js
@@ -14,16 +14,16 @@ Two or more series are stacked when their "stack" attribute is set to the same
 key (which can be any number or string or just "true"). To specify the default
 stack, you can set the stack option like this:
 
-    series: {
-        stack: null/false, true, or a key (number/string)
-    }
+	series: {
+		stack: null/false, true, or a key (number/string)
+	}
 
 You can also specify it for a single series, like this:
 
-    $.plot( $("#placeholder"), [{
-        data: [ ... ],
-        stack: true
-    }])
+	$.plot( $("#placeholder"), [{
+		data: [ ... ],
+		stack: true
+	}])
 
 The stacking order is determined by the order of the data series in the array
 (later series end up on top of the previous).
@@ -35,49 +35,154 @@ charts or filled areas).
 
 */
 
-/**
- * Patched version as per https://github.com/flot/flot/issues/326
- */
 (function ($) {
     var options = {
         series: { stack: null } // or number/string
     };
-
+    
     function init(plot) {
-
-        // will be built up dynamically as a hash from x-value to
-        var stackBases = {};
-
+        function findMatchingSeries(s, allseries) {
+            var res = null;
+            for (var i = 0; i < allseries.length; ++i) {
+                if (s == allseries[i])
+                    break;
+                
+                if (allseries[i].stack == s.stack)
+                    res = allseries[i];
+            }
+            
+            return res;
+        }
+        
         function stackData(plot, s, datapoints) {
             if (s.stack == null || s.stack === false)
                 return;
 
-            var newPoints = [];
-    
-            for (var i=0; i <  datapoints.points.length; i += 3) {
+            var other = findMatchingSeries(s, plot.getData());
+            if (!other)
+                return;
 
-                if (!stackBases[datapoints.points[i]]) {
-                    stackBases[datapoints.points[i]] = 0;
+            var ps = datapoints.pointsize,
+                points = datapoints.points,
+                otherps = other.datapoints.pointsize,
+                otherpoints = other.datapoints.points,
+                newpoints = [],
+                px, py, intery, qx, qy, bottom,
+                withlines = s.lines.show,
+                horizontal = s.bars.horizontal,
+                withbottom = ps > 2 && (horizontal ? datapoints.format[2].x : datapoints.format[2].y),
+                withsteps = withlines && s.lines.steps,
+                fromgap = true,
+                keyOffset = horizontal ? 1 : 0,
+                accumulateOffset = horizontal ? 0 : 1,
+                i = 0, j = 0, l, m;
+
+            while (true) {
+                if (i >= points.length)
+                    break;
+
+                l = newpoints.length;
+
+                if (points[i] == null) {
+                    // copy gaps
+                    for (m = 0; m < ps; ++m)
+                        newpoints.push(points[i + m]);
+                    i += ps;
+                }
+                else if (j >= otherpoints.length) {
+                    // for lines, we can't use the rest of the points
+                    if (!withlines) {
+                        for (m = 0; m < ps; ++m)
+                            newpoints.push(points[i + m]);
+                    }
+                    i += ps;
+                }
+                else if (otherpoints[j] == null) {
+                    // oops, got a gap
+                    for (m = 0; m < ps; ++m)
+                        newpoints.push(null);
+                    fromgap = true;
+                    j += otherps;
+                }
+                else {
+                    // cases where we actually got two points
+                    px = points[i + keyOffset];
+                    py = points[i + accumulateOffset];
+                    qx = otherpoints[j + keyOffset];
+                    qy = otherpoints[j + accumulateOffset];
+                    bottom = 0;
+
+                    if (px == qx) {
+                        for (m = 0; m < ps; ++m)
+                            newpoints.push(points[i + m]);
+
+                        newpoints[l + accumulateOffset] += qy;
+                        bottom = qy;
+                        
+                        i += ps;
+                        j += otherps;
+                    }
+                    else if (px > qx) {
+                        // we got past point below, might need to
+                        // insert interpolated extra point
+                        if (withlines && i > 0 && points[i - ps] != null) {
+                            intery = py + (points[i - ps + accumulateOffset] - py) * (qx - px) / (points[i - ps + keyOffset] - px);
+                            newpoints.push(qx);
+                            newpoints.push(intery + qy);
+                            for (m = 2; m < ps; ++m)
+                                newpoints.push(points[i + m]);
+                            bottom = qy; 
+                        }
+
+                        j += otherps;
+                    }
+                    else { // px < qx
+                        if (fromgap && withlines) {
+                            // if we come from a gap, we just skip this point
+                            i += ps;
+                            continue;
+                        }
+                            
+                        for (m = 0; m < ps; ++m)
+                            newpoints.push(points[i + m]);
+                        
+                        // we might be able to interpolate a point below,
+                        // this can give us a better y
+                        if (withlines && j > 0 && otherpoints[j - otherps] != null)
+                            bottom = qy + (otherpoints[j - otherps + accumulateOffset] - qy) * (px - qx) / (otherpoints[j - otherps + keyOffset] - qx);
+
+                        newpoints[l + accumulateOffset] += bottom;
+                        
+                        i += ps;
+                    }
+
+                    fromgap = false;
+                    
+                    if (l != newpoints.length && withbottom)
+                        newpoints[l + 2] += bottom;
                 }
 
-                // note that the values need to be turned into absolute y-values.
-                // in other words, if you were to stack (x, y1), (x, y2), and (x, y3), (each from different series, which is where stackBases comes in),
-                // you'd want the new points to be (x, y1, 0), (x, y1+y2, y1), (x,y1+y2+y3, y1+y2)
-                // generally, (x, thisValue + (base up to this point), + (base up tothis point))
-                newPoints[i] = datapoints.points[i];
-                newPoints[i+1] = datapoints.points[i+1] + stackBases[datapoints.points[i]];
-                newPoints[i+2] = stackBases[datapoints.points[i]];
-                stackBases[datapoints.points[i]] += datapoints.points[i+1];
+                // maintain the line steps invariant
+                if (withsteps && l != newpoints.length && l > 0
+                    && newpoints[l] != null
+                    && newpoints[l] != newpoints[l - ps]
+                    && newpoints[l + 1] != newpoints[l - ps + 1]) {
+                    for (m = 0; m < ps; ++m)
+                        newpoints[l + ps + m] = newpoints[l + m];
+                    newpoints[l + 1] = newpoints[l - ps + 1];
+                }
             }
-            datapoints.points = newPoints;
+
+            datapoints.points = newpoints;
         }
+        
         plot.hooks.processDatapoints.push(stackData);
     }
-
+    
     $.plot.plugins.push({
         init: init,
         options: options,
         name: 'stack',
-        version: '1.2-patch-326'
+        version: '1.2'
     });
 })(jQuery);
